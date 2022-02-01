a6510c1b7ddfdfac026274819aeca85550de8bb2
Seokju Na <seokju.me@gmail.com>
2022-02-01 17:34:19 +0900

    users worker working

--- users/src/lib.rs
+++ users/src/lib.rs
@@ -1,17 +1,22 @@
-mod create_user;
-
 use worker::*;
+
 use shared::routes::{health_route, version_route};
-use shared::utils::set_panic_hook;
+use shared::utils::wasm::set_panic_hook;
 
 #[event(fetch)]
-pub async fn main(req: Request, env: Env) -> Result<Response> {
+pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     let router = Router::new();
     set_panic_hook();
 
     router
         .get("/health", health_route)
         .get("/version", version_route)
+        .post_async("/users", |_req, ctx| async move {
+            let namespace = ctx.durable_object("USERS")?;
+            let stub = namespace.id_from_name("USERS")?.get_stub()?;
+
+            stub.fetch_with_request(_req).await
+        })
         .run(req, env)
         .await
 }
