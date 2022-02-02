35260b6b80fa95bdf51bb0d1c1715daf0a27aa7e
Seokju Na <seokju.me@gmail.com>
2022-02-02 22:30:11 +0900

    implement `GET /me`

--- users/src/lib.rs
+++ users/src/lib.rs
@@ -8,15 +8,18 @@ pub async fn main(req: Request, env: Env, _ctx: Context) -> Result<Response> {
     let router = Router::new();
     set_panic_hook();
 
+    let request_to_users = |_req: Request, ctx: RouteContext<()>| async move {
+        let namespace = ctx.durable_object("USERS")?;
+        let stub = namespace.id_from_name("USERS")?.get_stub()?;
+
+        stub.fetch_with_request(_req).await
+    };
+
     router
         .get("/health", health_route)
         .get("/version", version_route)
-        .post_async("/users", |_req, ctx| async move {
-            let namespace = ctx.durable_object("USERS")?;
-            let stub = namespace.id_from_name("USERS")?.get_stub()?;
-
-            stub.fetch_with_request(_req).await
-        })
+        .post_async("/users", request_to_users)
+        .get_async("/me", request_to_users)
         .run(req, env)
         .await
 }
